2b61f124d9cc8e71c03b01a11059c65e3e399ae0
Dmitry Demin <dmitry.demin@outlook.com>
2022-11-28 15:58:49 +0300

    Add license reference for network.proto

--- build.rs
+++ build.rs
@@ -1,9 +1,9 @@
 fn main() -> Result<(), anyhow::Error> {
-    println!("cargo:rerun-if-changed=src/network_protocol/network.proto");
+    println!("cargo:rerun-if-changed=src/network_protocol/proto/network.proto");
     protobuf_codegen::Codegen::new()
         .pure()
         .includes(["src/"])
-        .input("src/network_protocol/network.proto")
+        .input("src/network_protocol/proto/network.proto")
         .cargo_out_dir("proto")
         .run()
 }
